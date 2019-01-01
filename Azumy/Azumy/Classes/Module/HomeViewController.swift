d4b8bc87266f13c2993db779a7c3d1a407cd1553
azumy2416 <46301246+azumy2416@users.noreply.github.com>
2019-01-02 01:09:10 +0700

    Add files via upload

---
+++ Azumy/Azumy/Classes/Module/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Azumy
+//
+//  Created by Azmi on 02/01/19.
+//  Copyright © 2019 Azmi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Azumy/Azumy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Azumy
+//
+//  Created by Azmi on 02/01/19.
+//  Copyright © 2019 Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
